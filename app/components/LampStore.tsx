e30cd412c4d1513f1285daa3e0751c4b37ac0a42
SG-NaturalLackOfIntelligence <srikanth@accounton.io>
2025-02-08 08:46:36 +0100

    bla

--- app/components/LampStore.tsx
+++ app/components/LampStore.tsx
@@ -1,14 +1,19 @@
 "use client"
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import LivingRoom from "./rooms/LivingRoom"
-import Bedroom from "./rooms/Bedroom"
-import DiningRoom from "./rooms/DiningRoom"
+
+import DiningRoom from "/rooms/DiningRoom/"
+import Bedroom from "/rooms/Bedroom/"
+import LivingRoom from "/rooms/LivingRoom/"
+import Study from "/rooms/Study/"
+import Kitchen from "/rooms/Kitchen/"
+import Storage from "/rooms/Storage/"
+
 
 export default function LampStore() {
   return (
     <Tabs defaultValue="living-room" className="w-full">
-      <TabsList className="grid w-full grid-cols-3 h-auto">
+      <TabsList className="grid w-full grid-cols-3 h-auto sm:grid-cols-6">
         <TabsTrigger value="living-room" className="py-3 text-sm sm:text-base">
           Living Room
         </TabsTrigger>
@@ -17,6 +22,15 @@ export default function LampStore() {
         </TabsTrigger>
         <TabsTrigger value="dining-room" className="py-3 text-sm sm:text-base">
           Dining Room
+        </TabsTrigger>
+        <TabsTrigger value="study" className="py-3 text-sm sm:text-base">
+          Study
+        </TabsTrigger>
+        <TabsTrigger value="kitchen" className="py-3 text-sm sm:text-base">
+          Kitchen
+        </TabsTrigger>
+        <TabsTrigger value="storage" className="py-3 text-sm sm:text-base">
+          Storage
         </TabsTrigger>
       </TabsList>
       <TabsContent value="living-room">
@@ -27,6 +41,15 @@ export default function LampStore() {
       </TabsContent>
       <TabsContent value="dining-room">
         <DiningRoom />
+      </TabsContent>
+      <TabsContent value="study">
+        <Study />
+      </TabsContent>
+      <TabsContent value="kitchen">
+        <Kitchen />
+      </TabsContent>
+      <TabsContent value="storage">
+        <Storage />
       </TabsContent>
     </Tabs>
   )
